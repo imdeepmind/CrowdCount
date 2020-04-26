a9493b5850f30f472b189cd3c87be838b070db3e
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-04-26 17:15:38 +0530

    added code to read the images and process them

---
+++ constants.py
@@ -0,0 +1,6 @@
+LABELS_FILE = "data/labels.csv"
+IMAGES_PATH = "data/frames/"
+SAVE_FILE = "data/data.csv"
+
+IMAGE_SIZE = 555
+IMAGE_CHANNEL = 3
--- data.py
+++ data.py
@@ -1,9 +1,43 @@
+import cv2
+
 import pandas as pd
+import numpy as np
+
+from os import listdir
+from os.path import join
+from tqdm import tqdm
+
+from constants import LABELS_FILE, IMAGES_PATH, SAVE_FILE, IMAGE_SIZE, IMAGE_CHANNEL
 
 # Reading the label file
-labels_path = "data/labels.csv"
+data = pd.read_csv(LABELS_FILE)
+labels = data["count"].values
 
-data = pd.read_csv(labels_path)
+# Getting a list of all images
+images = listdir(IMAGES_PATH)
 
+# Setting the channel
+channel = 1 if IMAGE_CHANNEL == 3 else 0
+
+# Array to store all the images
+dataset = []
+
+# Iterating through the images
+for index, image in tqdm(enumerate(images)):
+	# Reading the image
+	img = cv2.imread(join(IMAGES_PATH, image), channel)
+
+	# Resizing the images
+	img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+
+	# Appending the images
+	img = np.append(img.flatten(), labels[index])	
+
+	# String the image and labels into main dataset array
+	dataset.append(img)
+
+
+# Cleaning some memory
+del data, labels, images, channel
 
 
